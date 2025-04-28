a282cb21cbd984a03b941ebe1828b41bbcbdc3b1
Bhushan1626 <bhushangandhakte7478@gmail.com>
2025-04-28 12:13:51 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple interactivity
+document.querySelectorAll('.menu li').forEach(item => {
+    item.addEventListener('click', () => {
+        alert(`You clicked on ${item.textContent}`);
+    });
+});
